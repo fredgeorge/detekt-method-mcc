876b3c540fc4e3d4b1349d5907095f338b3d96bb
GitHub Action <action@github.com>
2021-08-27 22:08:50 +0000

    Change package name from org.example.detekt to com.github.fredgeorge.detektmethodmcc

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     `maven-publish`
 }
 
-group = "org.example.detekt"
+group = "com.github.fredgeorge.detektmethodmcc"
 version = "1.0-SNAPSHOT"
 
 repositories {
--- src/main/kotlin/com/github/fredgeorge/detektmethodmcc/MyRule.kt
+++ src/main/kotlin/com/github/fredgeorge/detektmethodmcc/MyRule.kt
@@ -1,4 +1,4 @@
-package org.example.detekt
+package com.github.fredgeorge.detektmethodmcc
 
 import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
